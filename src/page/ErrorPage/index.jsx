381484c452b5d4cea1143962e1c8705a6e4a6c86
william <alown666@gmail.com>
2021-07-25 16:57:41 +0200

    Creation de la page 404

--- src/page/ErrorPage/index.jsx
+++ src/page/ErrorPage/index.jsx
@@ -1,10 +1,19 @@
 import { Component } from "react";
+import { Link } from 'react-router-dom'
+import './style.css'
 
 class ErrorPage extends Component
 {
     render()
     {
-        return <p>Error</p>
+        return (
+            <div className="Error_page">
+                <p className="Error_page_quatreCent">404</p>
+                <p className="Error_page_introuvable">Oups! La page que vous demandez n'existe pas.</p>
+                <Link className="Error_page_link" to="/">Retourner sur la page d’accueil</Link>
+            </div>
+
+        )
     }
 }
 export default ErrorPage
