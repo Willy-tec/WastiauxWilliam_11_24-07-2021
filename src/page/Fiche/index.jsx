1d14b959751653eb985a51ece9023b7afa1378b3
william <alown666@gmail.com>
2021-07-29 12:01:17 +0200

    Modification de la gestion d'erreur

--- src/page/Fiche/index.jsx
+++ src/page/Fiche/index.jsx
@@ -4,6 +4,7 @@ import data from '../../data/logements.json'
 import Galerie from "../../component/Galerie/Galerie";
 import DetailLogement from "../../component/DetailLogement";
 import Dropdown from "../../component/Dropdown";
+import ErrorPage from "../ErrorPage"
 import './style.css'
 
 class Fiche extends Component
@@ -13,27 +14,32 @@ class Fiche extends Component
         super(props)
         let indexLgt = +this.props.match.params.indexLogement
         if(typeof indexLgt === 'number' && indexLgt >= 0 && indexLgt < data.length) this.state = {lgt : data[indexLgt]}
-        else this.state = { lgt: data[0] }
+        else this.state = { lgt: "error" }
     }
 
     render()
     {
 
-        return <div className="fiche">
-            <Galerie listUrl={this.state.lgt.pictures }/>
-            <DetailLogement tagList={this.state.lgt.tags} rating={this.state.lgt.rating} host={this.state.lgt.host} title = {this.state.lgt.title} location = {this.state.lgt.location}>
-            </DetailLogement >
-            <div className="fiche_dropdown_section">
-                <Dropdown key = "fiche_dropdown_1" titre="Description">
-                    <p>{ this.state.lgt.description }</p>
-                </Dropdown>
-                <Dropdown key = "fiche_dropdown_2" titre="Équipements">
-                    {this.state.lgt.equipments.map((el, index) => <p key={`equip_${index}`}>{el}</p>) }
-                </Dropdown>
-            </div>
+        return (<>
+            {
+                this.state.lgt === "error" ?
+                    <ErrorPage/>
+                    :<div className="fiche">
+                        <Galerie listUrl={this.state.lgt.pictures }/>
+                        <DetailLogement tagList={this.state.lgt.tags} rating={this.state.lgt.rating} host={this.state.lgt.host} title = {this.state.lgt.title} location = {this.state.lgt.location}>
+                        </DetailLogement >
+                        <div className="fiche_dropdown_section">
+                            <Dropdown key = "fiche_dropdown_1" titre="Description">
+                                <p>{ this.state.lgt.description }</p>
+                            </Dropdown>
+                            <Dropdown key = "fiche_dropdown_2" titre="Équipements">
+                                {this.state.lgt.equipments.map((el, index) => <p key={`equip_${index}`}>{el}</p>) }
+                            </Dropdown>
+                        </div>
+                    </div>
+            }
             
-
-        </div>
+            </>)
     }
 }
 
